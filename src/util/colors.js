20c13bb5f2f6fa3411473d8bea815bad64fea24a
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-12 06:24:35 +0200

    Add colors library and utils

---
+++ src/util/colors.js
@@ -0,0 +1,32 @@
+import Color from 'color'
+
+export const valexHighlightBlue = Color('#cad3e6');
+export const valexLighterBlue = Color('#8499c6');
+export const valexLightBlue = Color('#48639c');
+export const valexBlue = Color('#4c4c9d');
+export const valexHighlightPurple = Color('#e9dde7');
+export const valexLighterPurple = Color('#c0a0bd');
+export const valexLightPurple = Color('#976391');
+export const valexPurple = Color('#712f79');
+export const valexHighlightOrange = Color('white');
+export const valexLightOrange = Color('#fcddcf');
+export const valexOrange = Color('#f7996e');
+
+export const white = Color('#fff');
+export const white1 = Color('#f8f8f8');
+export const white2 = Color('#f0f0f0');
+export const white3 = Color('#e8e8e8');
+
+export const veryLightGray = Color('#e0e0e0');
+export const lighterGray = Color('#d0d0d0');
+export const lightGray = Color('#cccccc');
+
+export const gray = Color('#a0a0a0');
+export const darkGray = Color('#888888');
+export const darkerGray = Color('#5f5f5f');
+export const veryDarkGray = Color('#444444');
+
+export const black = Color('#000');
+export const black1 = Color('#080808');
+export const black2 = Color('#181818');
+export const black3 = Color('#282828');

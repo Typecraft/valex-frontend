af6ef8620d297032c3c675715ab7a7abfe01f91b
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-13 18:02:06 +0200

    Add StaffOnly component

---
+++ src/views/login/StaffOnly.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+import login from 'state/login'
+import users from 'state/users'
+
+
+export class StaffOnly extends React.Component {
+  static propTypes = {
+    children: PropTypes.object,
+    loggedIn: PropTypes.bool,
+    className: PropTypes.string
+  }
+
+  static defaultProps = {
+
+  }
+
+  render = () => {
+    if (this.props.loggedIn && this.props.currentUser && this.props.currentUser.is_staff) {
+      return (
+        <div className={`staffonly ${this.props.className}`}>
+          {this.props.children}
+        </div>
+      )
+    }
+    return null
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    loggedIn: login.selectors.getIsLoggedIn(state),
+    currentUser: users.selectors.getCurrentUser(state)
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StaffOnly)

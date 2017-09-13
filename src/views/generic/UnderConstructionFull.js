eede21ecfbf4b689904341441cddd6b0423e4d5a
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-13 18:01:32 +0200

    Add UnderConstructionFull component

---
+++ src/views/generic/UnderConstructionFull.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import UnderConstruction from './UnderConstruction'
+
+import './UnderConstructionFull.css'
+
+const UnderConstructionFull = () => (
+  <div className="underconstructionfull valex-purple">
+    <UnderConstruction />
+  </div>
+)
+
+UnderConstructionFull.propTypes = {
+
+}
+
+UnderConstructionFull.defaultProps = {
+
+}
+
+export default UnderConstructionFull;

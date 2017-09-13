5faa07120aa0168bb0ffc40e85cb83af2b604ba8
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-13 08:41:25 +0200

    Add simple LemmaDetail view

--- src/views/lemmas/LemmaDetail.js
+++ src/views/lemmas/LemmaDetail.js
@@ -6,11 +6,13 @@ import lemmas from 'state/lemmas'
 
 import { ThreeBounce } from 'better-react-spinkit'
 
+import { Grid, Row, Col } from 'react-flexbox-grid'
+
 import './LemmaDetail.css'
 
 export class LemmaDetail extends React.Component {
   static propTypes = {
-
+    lemma: PropTypes.object
   }
 
   static defaultProps = {
@@ -40,10 +42,56 @@ export class LemmaDetail extends React.Component {
         </div>
       )
     } else {
+      const lemma = this.props.lemma
       return (
-      <div className="lemmadetail">
-        <h1>Exists!</h1>
-      </div>
+        <div className="lemmadetail">
+          <Grid className="lemmadetail__inner">
+            <Row className="mt-40">
+              <h1 className="light darker-gray">{lemma.lemma}</h1>
+              <div className="lemmadetail__edit darker-gray">Edit <i className="mdi mdi-pencil"></i></div>
+            </Row>
+            <Row className="mt-20">
+              <h3 className="light">Basic data</h3>
+              <Col xs={12}>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>Lemma</td>
+                      <td>{lemma.lemma}</td>
+                    </tr>
+                    <tr>
+                      <td>Language</td>
+                      <td>{lemma.language}</td>
+                    </tr>
+                    <tr>
+                      <td>Citation form</td>
+                      <td>{lemma.citform || 'This lemma has no registered Citation Form'}</td>
+                    </tr>
+                    <tr>
+                      <td>Comments</td>
+                      <td>{lemma.comments || 'This lemma has no comments'}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Col>
+            </Row>
+            <Row className="mt-20">
+              <h3 className="light">Meanings</h3>
+              <Col xs={12}>
+                {!lemma.meanings || lemma.meanings.length === 0 ?
+                  (<div>This lemma has no associated meanings</div>):
+                  (
+                    <div>
+                    {lemma.meanings.map(meaning => (
+                      <div key={meaning}>{meaning}</div>
+                    ))}
+                    </div>
+                  )
+                }
+              </Col>
+            </Row>
+          </Grid>
+        </div>
       )
     }
   }

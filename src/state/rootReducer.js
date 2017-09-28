b67aa29d4e4258bbea01e67fdb7e001b5d37a17a
Tormod Haugland <tormod.haugland@gmail.com>
2017-09-28 10:13:05 +0200

    Update rootReducer/Saga with new modules

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -4,10 +4,16 @@ import users from 'state/users'
 import login from 'state/login'
 import lemmas from 'state/lemmas'
 import meanings from 'state/meanings'
+import valenceframes from 'state/valenceframes'
+import meaningvalences from 'state/meaningvalences'
+import examples from 'state/examples'
 
 export default combineReducers({
   users: users.reducer,
   login: login.reducer,
   [lemmas.constants.NAME]: lemmas.reducer,
-  [meanings.constants.NAME]: meanings.reducer
+  [meanings.constants.NAME]: meanings.reducer,
+  [valenceframes.constants.NAME]: valenceframes.reducer,
+  [meaningvalences.constants.NAME]: meaningvalences.reducer,
+  [examples.constants.NAME]: examples.reducer,
 })
--- src/state/rootSaga.js
+++ src/state/rootSaga.js
@@ -4,12 +4,18 @@ import users from 'state/users'
 import login from 'state/login'
 import lemmas from 'state/lemmas'
 import meanings from 'state/meanings'
+import valenceframes from 'state/valenceframes'
+import meaningvalences from 'state/meaningvalences'
+import examples from 'state/examples'
 
 export default function*() {
   yield all([
     call(users.saga),
     call(login.saga),
     call(lemmas.saga),
-    call(meanings.saga)
+    call(meanings.saga),
+    call(meaningvalences.saga),
+    call(valenceframes.saga),
+    call(examples.saga),
   ])
 }

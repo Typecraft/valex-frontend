e0193cb348d0e6d09e0f42ed953a12b655169aa1
Tormod Haugland <tormod.haugland@gmail.com>
2017-10-04 07:32:58 +0200

    Add getLemmaOfTHeDay selector

--- src/state/lemmas/selectors.js
+++ src/state/lemmas/selectors.js
@@ -1,5 +1,6 @@
 import * as constants from './constants'
 
-export const getAll = state => state[constants.NAME]
+export const getAll = state => state[constants.NAME].all
+export const getLemmaOfTheDay = state => state[constants.NAME].ofTheDay
 export const getDetail = (state, props) => getById(state, props.match.params.lemmaId)
 export const getById = (state, id) => getAll(state)[id]

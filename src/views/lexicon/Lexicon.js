f233110587faee4183d46dfc2143b0480547de85
Tormod Haugland <tormod.haugland@gmail.com>
2017-10-23 10:14:01 +0200

    Add Lexicon Mobile version

--- src/views/lexicon/Lexicon.js
+++ src/views/lexicon/Lexicon.js
@@ -1,161 +1,32 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
-import queryString from 'query-string'
 
-import lemmas from 'state/lemmas'
-import searchIcon from 'assets/search-gray.svg'
-
-import { Grid, Row, Col } from 'react-flexbox-grid'
 import MediaQuery from 'react-responsive'
 
 import LexiconMobile from './LexiconMobile'
-import Checkbox from 'views/generic/forms/Checkbox'
-import AlphabetPaginator from 'views/pagination/AlphabetPaginator'
+import LexiconDesktop from './LexiconDesktop'
 
 import './Lexicon.css'
 
 export class Lexicon extends React.Component {
   static propTypes = {
-    lemmas: PropTypes.array,
-    currentPage: PropTypes.object,
-    char: PropTypes.string,
-    page: PropTypes.string,
-    lang: PropTypes.array
   }
 
   static defaultProps = {
 
   }
 
-  componentDidMount = () => {
-    this.props.changePage(this.props.page, this.props.char, this.props.lang)
-  }
-
-  handleLetterChange = (newChar) => {
-    const {
-      lang
-    } = this.props
-    this.props.changePage(1, newChar, lang)
-  }
-
-  handlePageChange = (newPage) => {
-    if (!newPage) {
-      newPage = this.props.page + 1
-    }
-
-    const {
-      char,
-      lang
-    } = this.props
-
-    this.props.changePage(newPage, char, lang)
-  }
-
-  handleLanguageChange = (language) => {
-    const {
-      char,
-      lang
-    } = this.props
-    let newLang
-
-    if (lang.includes(language)) {
-      newLang = lang.filter(x => x !== language)
-    } else {
-      newLang = lang.concat(language)
-    }
-    if (newLang.length === 0) {
-      newLang = ['none']
-    } else {
-      newLang = newLang.filter(x => x !== 'none')
-    }
-    this.props.changePage(1, char, newLang.join(","))
-  }
-
   render = () => {
-    const {
-      lemmas,
-      currentPage,
-      char,
-      page,
-      lang,
-    } = this.props
     return (
       <div className="lexicon">
         <MediaQuery query="(max-device-width: 1024px)">
-          <LexiconMobile {...this.props} />
+          <LexiconMobile />
         </MediaQuery>
         <MediaQuery query="(min-device-width: 1024px)">
-          <Grid>
-            <Row>
-              <Col lg={2}  className="lexicon__sidebar">
-                <div className="lexicon__search">
-                  <input type="text"/>
-                  <img src={searchIcon} alt=""/>
-                </div>
-                <div className="lexicon__languageselect">
-                  <div className="lexicon__languageoption">
-                    <Checkbox checked={lang.includes('deu')} onChange={() => this.handleLanguageChange('deu')} id="deutsch" label="German" defaultChecked={true} />
-                  </div>
-                  <div className="lexicon__languageoption">
-                    <Checkbox checked={lang.includes('nob')} onChange={() => this.handleLanguageChange('nob')} id="norwegian" label="Norwegian" />
-                  </div>
-                </div>
-              </Col>
-              <Col lg={10} className="lexicon__main">
-                <AlphabetPaginator
-                    next={currentPage.next}
-                    previous={currentPage.previous}
-                    onChangePage={this.handlePageChange}
-                    onLetterChange={this.handleLetterChange}
-                    currentCharacter={char}
-                    currentPage={page}>
-                  <div className="lexicon__table">
-                    {lemmas.map(lemma => (
-                      <div key={lemma.id} className="lexicon__cell">
-                        <Link to={`/app/lemmas/${lemma.id}`} className="resetlink">
-                          {lemma.lemma}
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                </AlphabetPaginator>
-              </Col>
-            </Row>
-          </Grid>
+          <LexiconDesktop />
         </MediaQuery>
       </div>
     )
   }
 }
 
-function mapStateToProps(state, ownProps) {
-  const search = queryString.parse(ownProps.location.search)
-  const page = search.page || 1
-  const char = search.char || 'a'
-  const lang = search.lang || 'deu'
-  const pageConf = `lemma__istartswith=${char}&language__in=${lang}`
-  const currentPage = lemmas.paginator.selectors.getPage(state, page, pageConf) || {}
-  return {
-    page,
-    char,
-    currentPage,
-    lang: lang.split(','),
-    lemmas: currentPage.results || []
-  }
-}
-
-function mapDispatchToProps(dispatch, ownProps) {
-  const location = ownProps.location.pathname
-  return {
-    changePage: (newPage, newChar, newLang) => {
-      const newPageConf = `lemma__istartswith=${newChar}&language__in=${newLang}`
-      dispatch(lemmas.paginator.actions.load(newPage, newPageConf))
-      const newUrl = location + `?page=${newPage}&char=${newChar}&lang=${newLang}`
-      ownProps.history.push(newUrl)
-    }
-  }
-}
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Lexicon))
+export default Lexicon
--- src/views/lexicon/LexiconMobile.js
+++ src/views/lexicon/LexiconMobile.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Link, withRouter } from 'react-router-dom'
+import _ from 'lodash'
+import queryString from 'query-string'
+
+import lemmas from 'state/lemmas'
 import searchIcon from 'assets/search-gray.svg'
 
 import './LexiconMobile.css'
 
 import FlagLanguageSelector from './FlagLanguageSelector'
+import ScrollAlphabetPaginator from 'views/pagination/ScrollAlphabetPaginator'
 
 export class LexiconMobile extends React.Component {
   static propTypes = {
@@ -12,34 +19,138 @@ export class LexiconMobile extends React.Component {
     currentPage: PropTypes.object,
     char: PropTypes.string,
     page: PropTypes.string,
-    lang: PropTypes.array
+    lang: PropTypes.array,
+    onChangePage: PropTypes.func
+  }
+
+  static defaultProps = {
+    onChangePage: _.identity
+  }
+
+  componentDidMount = () => {
+    this.props.changePage(1, this.props.char, this.props.lang)
+  }
+
+  handleLetterChange = (newChar) => {
+    const {
+      lang
+    } = this.props
+    this.props.changePage(1, newChar, lang)
+  }
+
+  handleLanguageChange = (language) => {
+    const {
+      char,
+      lang
+    } = this.props
+    let newLang
+
+    if (lang.includes(language)) {
+      newLang = lang.filter(x => x !== language)
+    } else {
+      newLang = lang.concat(language)
+    }
+    if (newLang.length === 0) {
+      newLang = ['none']
+    } else {
+      newLang = newLang.filter(x => x !== 'none')
+    }
+    this.props.changePage(1, char, newLang.join(","))
+  }
+
+  handlePageChange = (newPage) => {
+    if (!newPage) {
+      newPage = this.props.page + 1
+    }
+
+    const {
+      char,
+      lang
+    } = this.props
+
+    this.props.changePage(newPage, char, lang)
+  }
+
+  handleNextPage = (nextPage) => {
+    const {
+      page,
+      char,
+      lang,
+      changePage
+    } = this.props
+
+    changePage(parseInt(page, 10)+1, char, lang)
   }
 
   render() {
+    const {
+      currentPage,
+      char,
+      page,
+      lang,
+      lemmas
+    } = this.props
+
     return (
       <div className="lexiconmobile">
         <div className="lexicon__search">
           <input type="text"/>
           <img src={searchIcon} alt=""/>
-          <FlagLanguageSelector />
+          <FlagLanguageSelector
+              lang={lang}
+              onLanguageToggle={this.handleLanguageChange} />
         </div>
         <div className="lexiconmobile__main">
-          {this.props.lemmas.map(lemma => (
-            <div>{lemma.lemma}</div>
-          ))}
+          <ScrollAlphabetPaginator
+              currentPage={currentPage}
+              char={char}
+              page={page}
+              lang={lang}
+              useWindow={false}
+              onLoadMore={this.handleNextPage}
+              onLetterChange={this.handleLetterChange}
+          >
+            {lemmas.filter(_.identity).map(lemma => (
+              <div className="lexiconmobile__lemma">
+                <Link className="resetlink" to={`/app/lemmas/${lemma.id}/`}>
+                  {lemma.lemma}
+                </Link>
+              </div>
+            ))}
+          </ScrollAlphabetPaginator>
         </div>
       </div>
     )
   }
 }
-
-LexiconMobile.propTypes = {
-
+function mapStateToProps(state, ownProps) {
+  const search = queryString.parse(ownProps.location.search)
+  const page = search.page || 1
+  const pageParsed = parseInt(page, 10)
+  const char = search.char || 'a'
+  const lang = search.lang || 'deu'
+  const pageConf = `lemma__istartswith=${char}&language__in=${lang}`
+  const currentPage = lemmas.paginator.selectors.getPage(state, page, pageConf) || {}
+  const allLemmas = lemmas.paginator.selectors.getPagesUpToResults(state, pageParsed, pageConf)
+  return {
+    page,
+    char,
+    currentPage,
+    lang: lang.split(','),
+    lemmas: allLemmas || []
+  }
 }
 
-LexiconMobile.defaultProps = {
-
+function mapDispatchToProps(dispatch, ownProps) {
+  const location = ownProps.location.pathname
+  return {
+    changePage: (newPage, newChar, newLang) => {
+      const newPageConf = `lemma__istartswith=${newChar}&language__in=${newLang}`
+      dispatch(lemmas.paginator.actions.load(newPage, newPageConf))
+      const newUrl = location + `?page=${newPage}&char=${newChar}&lang=${newLang}`
+      ownProps.history.push(newUrl)
+    }
+  }
 }
 
-export default LexiconMobile;
-
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LexiconMobile))

50cabd1ae3d1b80bcb5120dacb9a9e112055be79
Tormod Haugland <tormod.haugland@gmail.com>
2017-10-23 10:13:16 +0200

    Add ScrollAlphabetPaginator component

---
+++ src/views/pagination/ScrollAlphabetPaginator.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import InfiniteScroller from 'react-infinite-scroller'
+import './ScrollAlphabetPaginator.css'
+
+const alphabet =
+  'abcdefghijklmnopqrstuvwxyzæøå'.split("")
+
+
+export const ScrollAlphabetPaginator = ({
+  children,
+  loadPage,
+  currentPage,
+  page,
+  onLoadMore,
+  useWindow,
+  currentCharacter,
+  onLetterChange
+}) => (
+  <div className="scrollalphabetpaginator">
+    <InfiniteScroller
+        pageStart={0}
+        useWindow={useWindow}
+        hasMore={!!currentPage.next}
+        loadMore={onLoadMore}>
+      {children}
+      {children}
+    </InfiniteScroller>
+    <div className="scrollalphabetpaginator__sidebar">
+      <div className="scrollalphabetpaginator__alphabet">
+        {alphabet.map(char => {
+          if (char === currentCharacter) {
+            return (
+              <span key={char} className="scrollalphabetpaginator__char gray thin">
+                {char}
+              </span>
+            )
+          } else {
+            return (
+              <span
+                  key={char}
+                  onClick={() => onLetterChange(char)}
+                  className="scrollalphabetpaginator__char thin pl-7 pr-7">
+                {char}
+              </span>
+            )
+          }
+        }
+        )}
+      </div>
+    </div>
+  </div>
+)
+
+ScrollAlphabetPaginator.propTypes = {
+
+}
+
+ScrollAlphabetPaginator.defaultProps = {
+
+}
+
+export default ScrollAlphabetPaginator;
